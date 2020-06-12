ee4d17d6ab48ae441f621253a283fcb05e092c88
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-12 11:22:28 -0600

    wip

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
 import { API_URL } from './resources/api'
 import Axios from 'axios'
 import Character from './components/Character'
 import './App.css';
 
+
+const CharacterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 60%;
+  max-height: 70vh;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  margin-top: 3%;
+  background: #C1C1C1;
+  opacity: 90%;
+  border-radius: 20px;
+  box-shadow: 0 0 5px black;
+`
 
 
 const App = () => {
@@ -11,7 +29,6 @@ const App = () => {
   // the state properties here.
   const [characterList, setCharacterList] = useState([])
   const [queryResults, setQueryResults] = useState([])
-  const [pages, setPages] = useState(0)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -20,9 +37,9 @@ const App = () => {
     Axios.get(`${API_URL}`)
       .then(response => {
         // console.log(response.data);
-        setPages(response.data.info.pages)
+        const pages = response.data.info.pages
 
-        for (let page=1; page<=1; page++){
+        for (let page=1; page<=2; page++){
           Axios.get(`${API_URL}?page=${page}`)
             .then(pageResponse => {
               pageResponse.data.results.forEach(resultItem => {
@@ -54,7 +71,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      {listCharacters()}
+      <CharacterContainer>
+        {listCharacters()}
+      </CharacterContainer>
     </div>
   );
 }
